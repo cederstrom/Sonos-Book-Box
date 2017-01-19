cbc6e348bc71175462273048d35527949f013455
Andreas Cederström <andreas.cederstrom@gmail.com>
2017-01-19 12:14:59 +0100

    Use correct spotify track and put it in the queue instead of playing direct. Also try harder to find the wanted player

--- book_box.py
+++ book_box.py
@@ -1,35 +1,73 @@
-# coding=utf-8
-from ConfigParser import NoOptionError
-from ConfigParser import SafeConfigParser
+# -*- coding: utf-8 -*-
+from ConfigParser import NoOptionError, SafeConfigParser
 from Getch import getch
+from soco.compat import quote_url
+from soco.data_structures import DidlItem, DidlResource
+from soco.music_services import MusicService
+from soco.music_services.accounts import Account
 import soco
+import sys
+import time
 
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 parser = SafeConfigParser()
 parser.read('config.txt')
-spotify_uri_pattern = 'x-sonos-spotify:{0}?sid=9'
 
-tv_rum = next(x for x in list(soco.discover()) 
-    if x.player_name == parser.get('sonos', 'room_name'))
+account = next(a for a in Account.get_accounts_for_service('2311')
+    if a.nickname == 'Andreas')
+spotify = MusicService('Spotify', account=account)
+
+room = None
+while(room is None):
+    try:
+        rooms = soco.discover()
+        room = next(x for x in list(rooms)
+            if x.player_name == parser.get('sonos', 'room_name'))
+        print("Found room: %s" % room.player_name)
+    except StopIteration as si:
+        print("Did not find wanted room. Trying again in 3 sec.")
+        time.sleep(3)
+
+
+def _get_queable_item(spotify_uri):
+    encoded_spotify_uri = quote_url(spotify_uri)
+    didl_item_id = '0fffffff{0}'.format(encoded_spotify_uri)
+    uri = spotify.sonos_uri_from_id(encoded_spotify_uri)
+    res = [DidlResource(uri=uri, protocol_info="DUMMY")]
+    return DidlItem(title="DUMMY",
+        # This is ignored. Sonos gets the title from the item_id
+        parent_id="DUMMY",  # Ditto
+        item_id=didl_item_id,
+        desc=spotify.desc,
+        resources=res)
+
 
 while(True):
     key = getch()
     print(key)
     if(key == '0'):
         print("Stop playback")
-        tv_rum.stop()
+        room.stop()
     elif(key == 'q'):
         print("Quitting")
         quit()
     else:
         try:
             spotify_uri = parser.get('songs', key)
-            sonos_uri = spotify_uri_pattern.format(spotify_uri)
-            name = parser.get('songs', key + '_name')
-            print("Playing: %s" % name)
-            tv_rum.volume = 20
-            tv_rum.clear_queue()
-            tv_rum.play_uri(sonos_uri)
+            track = _get_queable_item(spotify_uri)
+
+            room.stop()
+            room.volume = 20
+            #room.play_mode = 'REPEAT_ALL'
+            room.clear_queue()
+            room.add_to_queue(track)
+            room.play_from_queue(0)
+            
+            title = room.get_current_track_info().get('title')
+            print("Playing: %s" % title)
         except NoOptionError as no_option_exception:
             print(no_option_exception)
         except Exception as e:
